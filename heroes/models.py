b0845ccd748f695b56b95ec3d1ded70d37c0cc3f
itslewisndungu <itslewisndungu@gmail.com>
2022-09-08 12:23:19 +0300

    feat: Create Hero model

--- heroes/models.py
+++ heroes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from cities.models import CityModel
+
 # Create your models here.
+
+
+class HeroModel(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+
+    city = models.ForeignKey(
+        CityModel, on_delete=models.SET_NULL, blank=True, null=True
+    )
