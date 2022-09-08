2a749c92eee7386bab4f7a9f74a5ac9d55a63461
itslewisndungu <itslewisndungu@gmail.com>
2022-09-08 12:26:47 +0300

    Register hero and city models in to admin site

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import CityModel
 
-# Register your models here.
+admin.site.register(CityModel)
--- heroes/admin.py
+++ heroes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import HeroModel
+
 # Register your models here.
+
+admin.site.register(HeroModel)
